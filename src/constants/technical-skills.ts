2cff745e7da5f09bd14a527d6bd67c10c355bba4
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-01 12:00:46 +0100

    feat: add About Me section

---
+++ src/constants/technical-skills.ts
@@ -0,0 +1,17 @@
+export const technicalSkills: string[] = [
+  // Main languages/frameworks
+  'JavaScript/TypeScript',
+  'Dart',
+  'Flutter',
+  'Python',
+  'Kotlin',
+
+  // Frontend
+  'HTML',
+  'CSS',
+  'Tailwind CSS',
+
+  // Tools
+  'Git & GitHub',
+  'Android Studio'
+]
