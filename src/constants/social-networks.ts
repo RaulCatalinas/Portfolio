cabdff5c441ea6a7f9221ddf45f8f8a371c5e8d6
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-04 23:33:39 +0100

    chore: update AboutSection.astro, ProjectsSection.astro, and social-networks.ts files

--- src/constants/social-networks.ts
+++ src/constants/social-networks.ts
@@ -1,9 +1,10 @@
 import type { SocialNetwork } from '@/types/social-network'
 import { EMAIL_ADDRESS } from './email'
+import { GITHUB_URL } from './github'
 
 export const SOCIAL_NETWORKS: SocialNetwork[] = [
   {
-    url: 'https://github.com/RaulCatalinas',
+    url: GITHUB_URL,
     iconPath: '/icons/github.svg',
     iconAltText: 'GitHub Icon',
     invertColors: true
