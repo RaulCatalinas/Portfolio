8f0466b868b404dfd068f8ce36931e8b2206bfe9
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-05 22:07:05 +0100

    feat: add sitemap and robots.txt generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,25 @@ import { defineConfig } from 'astro/config'
 // Plugins
 import tailwindcss from '@tailwindcss/vite'
 
+// Integrations
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  site: 'https://raulcatalinas.vercel.app',
+
+  integrations: [sitemap(), robotsTxt()]
 })
