1fbb7f92cec23e500a385ffc0da96b111ccf8a50
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-27 01:44:37 +0100

    chore: organize components in subfolders

--- src/components/buttons/DownloadButton.tsx
+++ src/components/buttons/DownloadButton.tsx
@@ -1,5 +1,5 @@
 // Components
-import SelectOsDropdown from './SelectOsDropdown'
+import SelectOsDropdown from '../dropdowns/SelectOsDropdown'
 
 // Types
 import type { Os } from '@/types/projects'
